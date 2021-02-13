80617d59bd5313769f2f9b2b612d71a83597610d
idan <ghsi011@gmail.com>
2021-02-13 15:21:21 +0200

    works

--- public/farmServiceWorker.js
+++ public/farmServiceWorker.js
@@ -15,11 +15,28 @@ self.addEventListener('install', (event) => {
     )
 });
 // Listen for requests from
-self.addEventListener('fetch', () => {
-
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
 });
 
 // Activate the service worker
-self.addEventListener('activate', () => {
+self.addEventListener('activate', (event) => {
+    const cacheWhiteList = [];
+    cacheWhiteList.push(CACHE_NAME);
 
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if (!cacheWhiteList.includes(cacheName)) {
+                    return caches.delete(cacheName)
+                }
+            })
+        ))
+    )
 });
